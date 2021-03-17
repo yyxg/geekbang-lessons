79187cca9feef392fa3efe17ef9f017679f4b6cc
yyxg <441887221@qq.com>
2021-03-17 15:28:07 +0800

    jolokia-agent:

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/TestingListener.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/TestingListener.java
@@ -2,13 +2,16 @@ package org.geektimes.projects.user.web.listener;
 
 import org.geektimes.context.ComponentContext;
 import org.geektimes.projects.user.domain.User;
+import org.geektimes.projects.user.management.UserManager;
 import org.geektimes.projects.user.sql.DBConnectionManager;
 
+import javax.management.*;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.lang.management.ManagementFactory;
 import java.util.logging.Logger;
 
 /**
@@ -26,10 +29,32 @@ public class TestingListener implements ServletContextListener {
         dbConnectionManager.getConnection();
         testPropertyFromServletContext(sce.getServletContext());
         testPropertyFromJNDI(context);
-        testUser(dbConnectionManager.getEntityManager());
+//        testUser(dbConnectionManager.getEntityManager());
         logger.info("所有的 JNDI 组件名称：[");
         context.getComponentNames().forEach(logger::info);
         logger.info("]");
+        testMB();
+    }
+
+    private void testMB() {
+
+
+        try {
+            // 获取平台 MBean Server
+            MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+            // 为 UserMXBean 定义 ObjectName
+            ObjectName name = new ObjectName("jolokia:name=Myjolokia");
+            // 创建 UserMBean 实例
+            mBeanServer.registerMBean(new UserManager(new User()), name);
+        } catch (InstanceAlreadyExistsException e) {
+            e.printStackTrace();
+        } catch (MBeanRegistrationException e) {
+            e.printStackTrace();
+        } catch (NotCompliantMBeanException e) {
+            e.printStackTrace();
+        } catch (MalformedObjectNameException e) {
+            e.printStackTrace();
+        }
     }
 
     private void testPropertyFromServletContext(ServletContext servletContext) {
