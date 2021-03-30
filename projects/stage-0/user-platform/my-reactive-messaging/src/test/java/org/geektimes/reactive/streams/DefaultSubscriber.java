3fe594192cf2c492b510633f265edbeb11800d3d
yyxg <441887221@qq.com>
2021-03-30 18:04:50 +0800

    MicroProfile Reactive Messaging

--- projects/stage-0/user-platform/my-reactive-messaging/src/test/java/org/geektimes/reactive/streams/DefaultSubscriber.java
+++ projects/stage-0/user-platform/my-reactive-messaging/src/test/java/org/geektimes/reactive/streams/DefaultSubscriber.java
@@ -14,13 +14,22 @@ public class DefaultSubscriber<T> implements Subscriber<T> {
         this.subscription = s;
     }
 
+    /**
+     * 收到数据：0
+     * 收到数据：1
+     * 收到数据：2
+     * 本次数据发布已忽略，数据为：3
+     * 本次数据发布已忽略，数据为：4
+     * @param o
+     */
     @Override
     public void onNext(Object o) {
+        //打印提前一下就ok了
+        System.out.println("收到数据：" + o);
         if (++count > 2) { // 当到达数据阈值时，取消 Publisher 给当前 Subscriber 发送数据
             subscription.cancel();
             return;
         }
-        System.out.println("收到数据：" + o);
     }
 
     @Override
